e0166e3dc8d274ec22a79a811bb5a2ea4cc68f9e
Nate Riffe <inkblot@movealong.org>
2017-11-11 10:56:33 -0600

    Add cert auth support and fix SSL config

--- manifests/consul_template/service.pp
+++ manifests/consul_template/service.pp
@@ -10,11 +10,18 @@ class hashicorp::consul_template::service (
   $vault_address = $::hashicorp::consul_template::vault_address
   $vault_token = $::hashicorp::consul_template::vault_token
   $vault_auth_method = $::hashicorp::consul_template::vault_auth_method
+  $vault_auth_path = $::hashicorp::consul_template::vault_auth_path ? {
+    false   => $vault_auth_method,
+    default => $::hashicorp::consul_template::vault_auth_path
+  }
+  $vault_ssl_cert = $::hashicorp::consul_template::vault_ssl_cert
+  $vault_ssl_key = $::hashicorp::consul_template::vault_ssl_key
 
   if $vault and !$vault_token {
     include ::hashicorp::vault_env
     case $vault_auth_method {
-      'aws': { $service_command = "${install_dir}/vault-env -address=${vault_address} -method=${vault_auth_method} -- ${install_dir}/consul-template -config=${config_dir}" }
+      'aws': { $service_command = "${install_dir}/vault-env -address=${vault_address} -method=${vault_auth_method} -path=${vault_auth_path} -- ${install_dir}/consul-template -config=${config_dir}" }
+      'cert': { $service_command = "${install_dir}/vault-env -address=${vault_address} -method=${vault_auth_method} -path=${vault_auth_path} -client-cert=${vault_ssl_cert} -client-key=${vault_ssl_key} -- ${install_dir}/consul-template -config=${config_dir}" }
       default: { fail("TODO: support vault auth -method=${vault_auth_method}") }
     }
   } else {
